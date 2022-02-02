1a0642ada09ad52e6f20f847c95a4c6ab0bdecbc
Ashwin S K <ashwin.kolapadath24@gmail.com>
2022-02-02 16:04:08 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function changeToViolet()
+{
+    document.body.style.backgroundColor="violet";
+}
+function changeToIndigo()
+{
+    document.body.style.backgroundColor="indigo";
+}
+function changeToBlue()
+{
+    document.body.style.backgroundColor="blue";
+}
+function changeToGreen()
+{
+    document.body.style.backgroundColor="green";
+}
+function changeToYellow()
+{
+    document.body.style.backgroundColor="yellow";
+}
+function changeToOrange()
+{
+    document.body.style.backgroundColor="orange";
+}
+function changeToRed()
+{
+    document.body.style.backgroundColor="red";
+}
